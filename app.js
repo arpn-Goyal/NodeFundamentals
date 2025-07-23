6c05ebb75ea51616e1f4b2cdfb5e782a87b20d6d
arpn-Goyal <arpangoyal95@gmail.com>
2025-07-23 21:51:21 +0530

    Use COrs Helmet morgan

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-import express from 'express';
+import e from "express";
 
-const app = express();
-const port = 4000;
+const app = e();
+const port = 3000;
+// Built-in middleware
+app.use(e.json());
+// Use Tools (Middleware)
+app.use(cors());            // 1. Allow cross-origin requests
+app.use(helmet());          // 2. Secure your app with headers
+app.use(morgan('dev'));     // 3. Log HTTP requests
 
-app.get('/', (req, res)=>{
-    res.send('Hey Human ._. !!!')
+
+// Custom middleware
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.url}`);
+  next(); // move to next middleware/route
+}
+
+// Route that causes an error
+app.get('/error', (req, res, next) => {
+  next(new Error('Something went wrong!'));
+});
+
+// Error handling middleware (must have 4 params)
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).send('Internal Server Error!');
+});
+app.use(logger);
+
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Express.js Recap </h1>")
 })
+
 
 app.listen(port, ()=>{
     console.log(`http://localhost:${port}`)
